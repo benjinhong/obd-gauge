6ff1e504f27add53945e68b546b9ece42453b319
Benjamin Hong <88565431+benjinhong@users.noreply.github.com>
2022-10-14 20:34:41 -0700

    update
    added load parsing to parse8. center LED now reflects load percentage.
    
    added new program to read engine codes. no LED interaction besides welcome.

--- ELM_parse_engine_code/ELM_parse_engine_code.ino
+++ ELM_parse_engine_code/ELM_parse_engine_code.ino
@@ -12,8 +12,6 @@ AltSoftSerial obd;
 #define CENTER_COLOR CRGB::White
 #define CENTER_INDEX 15
 
-bool DEBUG_MODE = false;
-
 CRGB leds[NUM_LEDS];
 
 //misc
@@ -25,7 +23,6 @@ int MAP = 0;
 int ABP = 0;
 int LOAD = 0;
 int ERR = 0;
-int ERR_LAST = 0;
 int PID_SEL = 1;
 int lastBrightness = 0;
 
@@ -40,7 +37,7 @@ void setup() {
 
   FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
   
-  FastLED.setBrightness(5); //welcome brightness
+  FastLED.setBrightness(5); //initial brightness
   
   for (int i = 15; i >= 0; i--) //welcome sequence
   {
@@ -56,8 +53,6 @@ void setup() {
     FastLED.show();
     delay(5);
   }
-
-  FastLED.setBrightness(1); //initial brightness. assume night
   
   delay(100); //1E3 worked
   //obd.println("ATZ"); //set protocol 6
@@ -70,6 +65,8 @@ void setup() {
   delay(100);
   obd.println("ATS0"); //turn off spaces
   delay(100);
+  obd.println("ATL0"); //turn off spaces
+  delay(100);
   obd.readString(); //clear
   delay(100);
 }
@@ -78,57 +75,6 @@ void loop() {
   unsigned long currTime = millis();
   
   if (1) { //ERR == 0
-    //Serial.println("OK");
-    //Serial.print("Counter: ");
-    //Serial.prfintln(counter);
-    val = MAP - ABP;
-    if (ERR != 0)
-      val = 0;
-    
-    //Serial.println(val);
-    
-    if (val > 0) { //positive pressure
-      valScaled = map(val, 0, 70, 0, 15);
-    } else
-      valScaled = map(val, -10, 0, -10, 0);
-    //if (ERR != 0)
-    //  valScaled = 0;
-
-      //Serial.print("valScaled:" );
-      //Serial.println(valScaled);
-      //Serial.print("counter:" );
-      //Serial.println(counter);
-      
-    if (counter >= abs(valScaled-15)) { //go up (down) counter init 15
-      if (counter > 14)
-      {
-        //Serial.println("neg");
-        leds[counter-15] = CRGB::Black;
-      } else
-        leds[counter] = POS_COLOR;
-      counter--;
-    }
-    
-    if (counter < abs(valScaled-15)) { //go down (up)
-      if (counter > 14)
-      {
-        //Serial.println("neg");
-        leds[counter-15] = NEG_COLOR;
-      } else
-        leds[counter] = CRGB::Black;
-      counter++;
-    }
-    if (ERR == 0) //only set center if no errors. saw flickering in error states
-        if (LOAD >= 90 && LOAD < 135) {
-            leds[CENTER_INDEX] = CRGB::Green;
-        } else if (LOAD >= 135 && LOAD < 180) {
-            leds[CENTER_INDEX] = CRGB::Yellow;
-        } else if (LOAD >= 180)
-            leds[CENTER_INDEX] = CRGB::Red; 
-        else
-            leds[CENTER_INDEX] = CENTER_COLOR;
-
-    FastLED.show();
 
     //Serial.println(counter);
   } if (ERR == -1) { //CAN ERROR
@@ -145,33 +91,20 @@ void loop() {
     FastLED.show();
   }
   
-  if ((currTime - prevTime >= 50)) { //poll and process every 50 ms
+  if ((currTime - prevTime >= 1000)) { //poll and process every 50 ms
     getData();
     prevTime = currTime;
   }
 
-  if (currTime - prevTime2 >= 1000) //poll LDR and update brightness
+  if (currTime - prevTime2 >= 2000) //poll LDR and update brightness
   {
-    int brightness = map(analogRead(A0), 0, 512, 1, 20);
-
-    if (DEBUG_MODE)
-      Serial.println(brightness);
-
-    if (abs(brightness - lastBrightness) > 15)
-    {
-      Serial.print("brightness------------------------------");
-      Serial.println(brightness);
-      FastLED.setBrightness( brightness );
-      lastBrightness = brightness;
-    }
     
     prevTime2 = currTime;
   }
 
   if (currTime - prevTime3 >= 60E3) //poll ABP
   {
-    //Serial.println("poll ABP---------------------------------");
-    PID_SEL = 1;
+   
     prevTime3 = currTime;
   }
 
@@ -181,17 +114,16 @@ void loop() {
    */
 }
 
+
+
 int getData() { //whatever that is in the character array is still represented in hex.
   char response[32] = {};
-  char MAParr[3] = {};
-  char LOADarr[3] = {};
-  if (PID_SEL == 1) //1 = poll ABP + MAP
-  {
-    obd.println("010B331"); //MAP + ABP //01 0B 33 1
+
+    obd.println("031"); //MAP + ABP
     //Serial.println("Requesting MAP+ABP");
-  }
-  else
-    obd.println("010B041"); //MAP ONLY is 010B1. currently polling MAP + LOAD
+  
+  
+    //obd.println(""); //MAP + LOAD 010B041
   
   //delay(50); //wait for buffer to fill
   int index = 0; //reset array index
@@ -209,34 +141,19 @@ int getData() { //whatever that is in the character array is still represented i
   
   //error checking
   if (response[0] == 'C') { //CAN ERROR
+    //Serial.println("CAN ERROR");
     ERR = -1;
-    if (ERR_LAST != ERR)
-      Serial.println("CAN ERROR");
-    ERR_LAST = ERR;
   } else if (response[0] == 'N') { //NO DATA
+    //Serial.println("NO DATA");
     ERR = -2;
-    if (ERR_LAST != ERR)
-      Serial.println("NO DATA");
-    ERR_LAST = ERR;
   } else if (response[0] == 'S') { //STOPPED
+    //Serial.println("STOPPED");
     ERR = -3;
-    if (ERR_LAST != ERR)
-      Serial.println("STOPPED");
-    ERR_LAST = ERR;
   } else if (response[0] != '4') { //BAD FRAME
+    //Serial.println("BAD FRAME");
     ERR = -4;
-    if (ERR_LAST != ERR)
-      Serial.println("BAD FRAME");
-    ERR_LAST = ERR;
   } else {
     ERR = 0;
-    MAParr[0] = response[4]; //4
-    MAParr[1] = response[5]; //5
-  
-    LOADarr[0] = response[8]; //8
-    LOADarr[1] = response[9]; //9
-
-    MAP = strtol(&MAParr[0], NULL, 16);
     
     //worked in theory but not in practice. turns out response is one cycle late. 
     /*if (PID_SEL == 1) { //ABP
@@ -245,30 +162,25 @@ int getData() { //whatever that is in the character array is still represented i
       PID_SEL = 0;
     }*/
     //this will wait until it sees 33. if it does, parse as expected.
-    if (response[6] == '3')
+    /*if (response[6] == '3')
     {
       ABP = strtol(&LOADarr[0], NULL, 16);
     } else {
       LOAD = strtol(&LOADarr[0], NULL, 16);
-    }
-
-
-    PID_SEL = 0;
+    }*/
     
     //print array contents
     //41 0B 64 normal response
-    if (DEBUG_MODE) {
-        for (int i = 0; i < index; i++)  {
+
+    /*
+    If you want to read the ready-to-receive character ">" then look for 0x3E (ASCII).
+    */
+
+    for (int i = 2; i < index; i++)  {
+      if (response[i] != 0x3E)
         Serial.print(response[i]);
-      }
-        //Serial.println();
-        //Serial.print("TURBO: ");
-        //Serial.println(val);
-        Serial.print("LOAD: ");
-        Serial.println(LOAD);
-        //Serial.print("ABP: ");
-        //Serial.println(ABP);      
+      
     }
-    
+    Serial.println();
   }
 }
